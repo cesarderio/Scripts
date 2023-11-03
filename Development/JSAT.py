6abf9cdbdb889007dbac481ebe9aa1dd06391605
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-11-02 19:24:25 -0700

    adding ducky scripts to development

---
+++ Development/JSAT.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Script Name:                  
+# Author:                       Raphael Chookagian
+# Date of latest revision:      09/21/2023
+# Purpose:                      Job Search/scrape tool
+# add features to search and apply?
+
+# install dependencies
+# pip install scrapy beautifulsoup4 requests
+
